bcfd8268ff5246d01b6ca478d20076d83b075c89
wzningjie <514309817@qq.com>
2023-06-25 02:55:42 +0800

    alte

--- lib/src/behaviors/legend/legend_content_builder.dart
+++ lib/src/behaviors/legend/legend_content_builder.dart
@@ -47,6 +47,9 @@ abstract class BaseLegendContentBuilder implements LegendContentBuilder {
   Widget build(BuildContext context, common.LegendState legendState,
       common.Legend legend,
       {bool showMeasures}) {
+	if(legendState.legendEntries.isEmpty){
+      return legendLayout.build(context, []);
+    }
     final entryWidgets = legendState.legendEntries.map((entry) {
       var isHidden = false;
       if (legend is common.SeriesLegend) {
